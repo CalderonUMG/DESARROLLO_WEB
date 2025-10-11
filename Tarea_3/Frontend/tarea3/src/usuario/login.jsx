6fbbab3c2c102cd47ecb4954b8c873379ee2bf33
CalderonUMG <mcalderonm17@miumg.edu.gt>
2025-10-11 08:17:10 -0600

    solucion de error

--- Tarea_3/Frontend/tarea3/src/usuario/login.jsx
+++ Tarea_3/Frontend/tarea3/src/usuario/login.jsx
@@ -17,7 +17,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:4000/api/login", {
+      const response = await fetch("https://desarrollo-web-1nh5.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(credentials),
--- Tarea_3/Frontend/tarea3/src/usuario/registro.jsx
+++ Tarea_3/Frontend/tarea3/src/usuario/registro.jsx
@@ -20,7 +20,7 @@ export default function Registro() {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:4000/api/registro", {
+            const response = await fetch("https://desarrollo-web-1nh5.onrender.com/api/registro", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(user),
